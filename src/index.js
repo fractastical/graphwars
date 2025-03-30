a0dacbaef0c09c75ffb6e3e4e9d27d47e1e3fabc
Joel Dietz <jdietz+github@gmail.com>
2025-03-30 10:17:47 -0700

    working version

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GraphWarsGame from './GraphWarsGame';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GraphWarsGame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
